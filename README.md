6c8675001ba4ca8f6cc9efe01576b43a487d5840
Arthur Lui <luiarthur@gmail.com>
2016-09-29 19:17:43 -0700

    yes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mcmc.jl
 MCMC stuff for Julia
+
+# To do
+
+- check that the Require file has the right format and version dependencies
+- reference Bayes.jl and julia_practice for how to do lazy and tco
--- src/mcmc.jl
+++ src/mcmc.jl
@@ -1,5 +1,50 @@
 module MCMC
+export Specifications, gibbs
 
-type State
+#=
+using Lazy
+function logfact(n)
+ @bounce function loop(N, acc) 
+   N == 0? acc : loop(N-1, log(N) + acc)
+ end
+ x = loop(n,0.0)
+ println(x)
+ x
+end
+
+=#
+
+typealias State Dict{String,Any}
+
+type Specifications
+  init::Dict{String,Any}
+  fcs::Dict{String,Any}
+end
+
+function gibbs(specs::Specifications,B::Int,burn::Int)
+  out = [ State for i in 1:(B+burn) ]
+
+  init = specs.init
+  fcs = specs.fcs
+  params = 
+
+  function update(currState::State) 
+    function loop
+    end
+
+  end
+
+  out
+end
 
 end
+
+#= Tests
+f(x) = x + 1
+typeof(f)
+
+init = Dict(:mu => 1)
+fcs = Dict(:mu => f)
+specs = Specifications(init,fcs)
+=#
+
