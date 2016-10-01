001f4da42b8f51a365dfdfba7724ff6dced684c9
Arthur Lui <luiarthur@gmail.com>
2016-09-30 17:03:26 -0700

    yes

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -1,3 +1,9 @@
+"""
+My MCMC stuff
+
+Currently the only method is gibbs. Eventually, I would
+like to add a Metropolis Hastings method.
+"""
 module MCMC
 
   export Specifications, gibbs, State
@@ -10,6 +16,58 @@ module MCMC
     fcs::FullConditionals
   end
 
+  """
+  Runs gibbs sampler with specifications (spec), samplers (B), 
+  and burn-in (burn). 
+
+
+  # Example:
+
+  ```julia
+  using Distributions
+
+  const n = 1000
+  const μ = 5
+  const σ² = 2
+  const y = randn(n) * sqrt(σ²) + μ
+  const ybar = mean(y)
+  const a = 2
+  const b = 1
+
+  rig(shape::Float64,rate::Float64) = 1 / rand(Gamma(shape,1/rate))
+
+  function updateσ²(param)
+    mu = param["mu"]
+    ss = sum((y - mu).^2)
+    rig(a+n/2, b+ss/2)
+  end
+
+  function updateμ(param)
+    s2 = param["sig2"]
+    randn() * sqrt(s2/n) + ybar
+  end
+
+  const specs = Specifications(
+    # init:
+    Dict(
+      "mu" => 0,
+      "sig2" => 1
+    ),
+    # full conditionals:
+    Dict(
+      "mu" => updateμ,
+      "sig2" => updateσ²
+    )
+  )
+
+  B = 10000
+  @time out = gibbs(specs,B,1000)
+
+  postMu = [out[i]["mu"] for i in 1:B]
+  postSig2 = [out[i]["sig2"] for i in 1:B]
+
+  ```
+  """
   function gibbs(specs::Specifications,B::Int,burn::Int)
     const out = Array{State,1}( (B+burn) )
 
