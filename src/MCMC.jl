5844db837f87cc895c385643870d29bc2a731c97
Arthur Lui <luiarthur@gmail.com>
2016-10-01 22:58:52 -0700

    yes

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -6,91 +6,56 @@ like to add a Metropolis Hastings method.
 """
 module MCMC
 
-  export Specifications, gibbs, State
-
-  typealias State Dict{Symbol,Any}
-  typealias FullConditionals Dict{Symbol,Any}
-
-  immutable Specifications
-    init::State
-    fcs::FullConditionals
-  end
-
+  export gibbs, GibbsState
   """
   Runs gibbs sampler with specifications (spec), samples (B), 
   and burn-in (burn). 
-
 
   # Example:
 
   ```julia
   using Distributions
 
+  # Generate data
   const n = 1000
-  const μ = 5
-  const σ² = 2
-  const y = randn(n) * sqrt(σ²) + μ
-  const ybar = mean(y)
-  const a = 2
-  const b = 1
+  const (mu,sig2) = (5,2)
+  const y = rand(Normal(mu,sqrt(sig2)),n)
 
+  # precomputes
+  const ybar = mean(y)
+  const (sig2_a, sig2_b) = (2.0, 1.0)
   rig(shape::Float64,rate::Float64) = 1 / rand(Gamma(shape,1/rate))
 
-  function updateσ²(param)
-    mu = param[:mu]
-    ss = sum((y - mu).^2)
-    rig(a+n/2, b+ss/2)
+  # Define State
+  immutable State
+    mu::Float64
+    sig2::Float64
   end
 
-  function updateμ(param)
-    s2 = param[:sig2]
-    randn() * sqrt(s2/n) + ybar
+  # define an update function for State
+  function update(state::State)
+    const newMu = rand(Normal(ybar,sqrt(state.sig2/n)))
+    const newSig2 = rig(sig2_a+n/2, sig2_b + sum((y-newMu).^2)/2)
+    State(newMu,newSig2)
   end
 
-  const specs = Specifications(
-    # init:
-    Dict(
-      :mu => 0,
-      :sig2 => 1
-    ),
-    # full conditionals:
-    Dict(
-      :mu => updateμ,
-      :sig2 => updateσ²
-    )
-  )
+  # run gibbs
+  @time out = gibbs(State(0,1),update,10000,1000);
 
-  B = 10000
-  @time out = gibbs(specs,B,1000)
-
-  postMu = [out[i][:mu] for i in 1:B]
-  postSig2 = [out[i][:sig2] for i in 1:B]
+  ## post processing
+  const postMu = map(o -> o.mu, out)
+  const postSig2 = map(o -> o.sig2, out)
 
   ```
   """
-  function gibbs(specs::Specifications,B::Int,burn::Int)
-    const out = Array{State,1}( (B+burn) )
 
-    const init = specs.init
-    const fcs = specs.fcs
-    const params = keys(init)
-    const d = length(params)
-
-    function update(currState::State)
-      newState = Dict( collect(currState) ) # important step!
-      for p in params
-        newState[p] = fcs[p](newState)
-      end
-      newState
-    end
-
+  function gibbs{T}(init::T, update, B::Int, burn::Int)
+    const out = Array{T,1}( (B+burn) )
     out[1] = init
     for i in 2:(B+burn)
       out[i] = update(out[i-1])
     end
-
-    out[(burn+1):end]
+    out[ (burn+1):end ]
   end
-
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,53 +1,42 @@
 using MCMC
 using Base.Test
 
+# Define immutable class State
+immutable State
+  mu::Float64
+  sig2::Float64
+end
+
 @testset "Posterior Means" begin
   srand(1)
   using Distributions
 
+  # Generate data
   const n = 1000
-  const μ = 5
-  const σ² = 2
-  const y = randn(n) * sqrt(σ²) + μ
-  const ybar = mean(y)
-  const a = 2
-  const b = 1
+  const (mu,sig2) = (5,2)
+  const y = rand(Normal(mu,sqrt(sig2)),n)
 
+  # precomputes
+  const ybar = mean(y)
+  const (sig2_a, sig2_b) = (2.0, 1.0)
   rig(shape::Float64,rate::Float64) = 1 / rand(Gamma(shape,1/rate))
 
-  function updateσ²(param)
-    mu = param[:mu]
-    ss = sum((y - mu).^2)
-    rig(a+n/2, b+ss/2)
+
+  # define an update function for State
+  function update(state::State)
+    const newMu = rand(Normal(ybar,sqrt(state.sig2/n)))
+    const newSig2 = rig(sig2_a+n/2, sig2_b + sum((y-newMu).^2)/2)
+    State(newMu,newSig2)
   end
 
-  function updateμ(param)
-    s2 = param[:sig2]
-    randn() * sqrt(s2/n) + ybar
-  end
+  # run gibbs
+  @time out = gibbs(State(0,1),update,10000,1000);
 
-  const specs = Specifications(
-    # init:
-    Dict(
-      :mu => 0,
-      :sig2 => 1
-    ),
-    # full conditionals:
-    Dict(
-      :mu => updateμ,
-      :sig2 => updateσ²
-    )
-  )
-
-  B = 10000
-  @time out = gibbs(specs,B,1000)
-
-  postMu = [out[i][:mu] for i in 1:B]
-  postSig2 = [out[i][:sig2] for i in 1:B]
-
-  @test true
-  @test abs(mean(postMu) - μ) < .05
-  @test abs(mean(postSig2) - σ²) < .05
+  ## post processing
+  const postMu = map(o -> o.mu, out)
+  const postSig2 = map(o -> o.sig2, out)
+  @test abs(mean(postMu)-mu) < .05
+  @test abs(mean(postSig2)-sig2) < .05
 end
 
 #= Personal tests:
