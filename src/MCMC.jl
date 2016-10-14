c28df8795105b9f1750f2c8a3f86ca0dba927d82
Arthur Lui <luiarthur@gmail.com>
2016-10-14 09:36:01 -0700

    remove indents, added progress bar

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -7,87 +7,95 @@ like to add a Metropolis Hastings method.
 """
 module MCMC
 
-  import Distributions
-  export gibbs, mh_normal
-  """
-  Runs gibbs sampler with specifications (spec), samples (B), 
-  and burn-in (burn). 
+import Distributions
+export gibbs, mh_normal
+"""
+Generalized Gibbs sampler.
 
-  # Example:
+# Arguments
+* `init`: Initial values for gibbs sampler
+* `update`: function which takes a state::State as the only input and returns the update
+* `B`: number of MCMC samples desired
+* `burn`: burn-in
+* `printFreq`: shows progress bar every printFreq%. (default: 0 => don't print progress)
 
-  ```julia
-  using Distributions
+# Example:
 
-  # Generate data
-  const n = 1000
-  const (mu,sig2) = (5,2)
-  const y = rand(Normal(mu,sqrt(sig2)),n)
+```julia
+using Distributions
 
-  # precomputes
-  const ybar = mean(y)
-  const (sig2_a, sig2_b) = (2.0, 1.0)
-  rig(shape::Float64,rate::Float64) = 1 / rand(Gamma(shape,1/rate))
+# Generate data
+const n = 1000
+const (mu,sig2) = (5,2)
+const y = rand(Normal(mu,sqrt(sig2)),n)
 
-  # Define State
-  immutable State
-    mu::Float64
-    sig2::Float64
-  end
+# precomputes
+const ybar = mean(y)
+const (sig2_a, sig2_b) = (2.0, 1.0)
+rig(shape::Float64,rate::Float64) = 1 / rand(Gamma(shape,1/rate))
 
-  # define an update function for State
-  function update(state::State)
-    const newMu = rand(Normal(ybar,sqrt(state.sig2/n)))
-    const newSig2 = rig(sig2_a+n/2, sig2_b + sum((y-newMu).^2)/2)
-    State(newMu,newSig2)
-  end
+# Define State
+immutable State
+  mu::Float64
+  sig2::Float64
+end
 
-  # run gibbs
-  @time out = gibbs(State(0,1),update,10000,1000);
+# define an update function for State
+function update(state::State)
+  const newMu = rand(Normal(ybar,sqrt(state.sig2/n)))
+  const newSig2 = rig(sig2_a+n/2, sig2_b + sum((y-newMu).^2)/2)
+  State(newMu,newSig2)
+end
 
-  ## post processing
-  const postMu = map(o -> o.mu, out)
-  const postSig2 = map(o -> o.sig2, out)
+# run gibbs
+@time out = gibbs(State(0,1),update,10000,1000);
 
-  ```
-  """
+## post processing
+const postMu = map(o -> o.mu, out)
+const postSig2 = map(o -> o.sig2, out)
 
-  function gibbs{T}(init::T, update, B::Int, burn::Int)
-    const out = Array{T,1}( (B+burn) )
-    out[1] = init
-    for i in 2:(B+burn)
-      out[i] = update(out[i-1])
+```
+"""
+
+function gibbs{T}(init::T, update, B::Int, burn::Int; printFreq::Int=0)
+  const out = Array{T,1}( (B+burn) )
+  out[1] = init
+  for i in 2:(B+burn)
+    out[i] = update(out[i-1])
+    if printFreq > 0 && i % div(B,printFreq) == 0
+      print("\rProgress: ",i,"/",B+burn)
     end
-    out[ (burn+1):end ]
+  end
+  out[ (burn+1):end ]
+end
+
+"""
+Univariate Metropolis Hastings step (with Normal proposal)
+
+    mh_normal(curr, loglike_plus_logprior, acc::Int, candSig::Real; 
+              inbounds = x-> -Inf<x<Inf)
+
+# Arguments
+* `curr`: current value of parameter
+* `loglike_plus_logprior`: log-likelihoodd plus log-prior as function of the parameter to be updated
+* `acc`: The current acceptance count 
+* `candSig`: The sd of the normal proposal
+* `inbounds`: A function which checks if the current parameter is in the support
+"""
+function mh_normal(curr, loglike_plus_logprior, acc::Int, candSig::Real; 
+                   inbounds = x-> -Inf<x<Inf)
+  # need to do autotune
+
+  const cand = rand(Distributions.Normal(curr,candSig))
+
+  if inbounds(cand)
+    u = log(rand())
+    p = loglike_plus_logprior(cand) - loglike_plus_logprior(curr)
+    p>u ?  (cand,acc+1) : (curr,acc)
+  else
+    (curr,acc)
   end
 
-  """
-  Univariate Metropolis Hastings step (with Normal proposal)
-
-      mh_normal(curr, loglike_plus_logprior, acc::Int, candSig::Real; 
-                inbounds = x-> -Inf<x<Inf)
-
-  # Arguments
-  * `curr`: current value of parameter
-  * `loglike_plus_logprior`: log-likelihoodd plus log-prior as function of the parameter to be updated
-  * `acc`: The current acceptance count 
-  * `candSig`: The sd of the normal proposal
-  * `inbounds`: A function which checks if the current parameter is in the support
-  """
-  function mh_normal(curr, loglike_plus_logprior, acc::Int, candSig::Real; 
-                     inbounds = x-> -Inf<x<Inf)
-    # need to do autotune
-
-    const cand = rand(Distributions.Normal(curr,candSig))
-
-    if inbounds(cand)
-      u = log(rand())
-      p = loglike_plus_logprior(cand) - loglike_plus_logprior(curr)
-      p>u ?  (cand,acc+1) : (curr,acc)
-    else
-      (curr,acc)
-    end
-
-  end
-
+end
 
 end # module
